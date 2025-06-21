37ae60c5c8435659def4445662b08a6548292a6a
Daniel Caitano <danielcaitano9@hotmail.com>
2025-06-20 23:09:38 -0300

    my second commit

--- README.md
+++ README.md
@@ -8,13 +8,22 @@ docker logs node-app
 docker volume ls
 docker volume rm nameVolume
 docker volume prune
-
+docker inspect node-docker-mongo-1
+docker network ls
+docker network inspect node-docker_default
 
 cat node index.js
 touch testefile
 printenv
+ping mongo
 
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
-docker-compose up --help
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build --no-deps node-app
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml down -v
 
+docker exec -it node-docker-mongo-1 mongosh -u "sanjeev" -p "mypassword"
+mongosh -u "sanjeev" -p "mypassword"
+db
+use mydb
+show dbs
+db.books.insertOne({"name": "harry potter"})
+db.books.find()
--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express')
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
+const postRoutes = require('./routes/postRoutes')
+
 const app = express()
+let attemptsConnection = 0
+
+const connectWithRetry = () => {
+    const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+    mongoose
+        .connect(mongoURL)
+        .then(() => console.log('succesfully connected to DB'))
+        .catch((e) => {
+            console.log(e)
+            setTimeout(() => {
+                attemptsConnection++
+                if(attemptsConnection <= 6) connectWithRetry()
+            }, 5000);
+        });
+}
 
 const port = process.env.PORT || 3000
+connectWithRetry()
 
-app.get("/", (req, res)=>{
+app.use(express.json())
+
+app.get("/", (req, res) => {
     res.send("<h2>Hi there</h2>")
 })
 
-app.listen(port, ()=>console.log(`listening on port ${port}`))
+app.use("/api/v1/posts", postRoutes)
+
+app.listen(port, () => console.log(`listening on port ${port}`))
